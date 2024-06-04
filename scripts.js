19a55245e744d27ffc7bddaee18f3f5e0adc220a
Kory Plotts <koryplotts@yahoo.com>
2024-06-04 12:00:28 -0700

    new file:   about.html 	new file:   blog.html 	new file:   images/ka.jpg 	new file:   index.html 	new file:   myscript.css 	new file:   scripts.js 	new file:   shop.html

---
+++ scripts.js
@@ -0,0 +1,7 @@
+document.getElementById('searchButton').addEventListener('click', function () {
+    var searchInput = document.getElementById('searchInput').value.toLowerCase();
+    // Here, you can implement your search functionality
+    // For example, you can filter your products based on the search input
+    // and display the results on the page
+    console.log('Searching for:', searchInput);
+});
